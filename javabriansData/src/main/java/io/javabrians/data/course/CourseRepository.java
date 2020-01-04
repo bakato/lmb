11939004eb3c4063da1bcd674cef6cfc5794b2b8
i261171 <laurent_bona@bcbstx.com>
2020-01-03 18:55:55 -0600

    Comment on CrudRepository interface

--- javabriansData/src/main/java/io/javabrians/data/course/CourseRepository.java
+++ javabriansData/src/main/java/io/javabrians/data/course/CourseRepository.java
@@ -6,6 +6,11 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface CourseRepository extends CrudRepository<Course, String> {
 	
+	// to create custom method:
+	// key here is take  findBy + propertie name of Entity module file ( i.e: id, name, description )  
+	// --> findByName, findById, findByDescription, findByTopicId 
+	// CrudRepository will take care of query and provides the right result.
+	
 	public List<Course> findByTopicId(String topicId);
 	
 
