183df61a47754b6a0622e60e9dac3a599a9a1d49
i261171 <laurent_bona@bcbstx.com>
2020-01-12 01:14:35 -0600

    check in

--- javabriansData/src/main/java/io/javabrians/data/course/CourseRepository.java
+++ javabriansData/src/main/java/io/javabrians/data/course/CourseRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.CrudRepository;
 public interface CourseRepository extends CrudRepository<Course, String> {
 	
 	// to create custom method:
-	// key here is take  findBy + propertie name of Entity module file ( i.e: id, name, description )  
+	// key here is take:  findBy + propertie name of Entity module file ( i.e: id, name, description )  
 	// --> findByName, findById, findByDescription, findByTopicId 
 	// CrudRepository will take care of query and provides the right result.
 	
