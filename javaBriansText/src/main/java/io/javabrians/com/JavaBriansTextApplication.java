f87383568835ef933114c97ca61adbd71209eea4
i261171 <laurent_bona@bcbstx.com>
2020-01-13 22:10:30 -0600

    saved JavaBriansTextApplication.java

---
+++ javaBriansText/src/main/java/io/javabrians/com/JavaBriansTextApplication.java
@@ -0,0 +1,14 @@
+package io.javabrians.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBriansTextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBriansTextApplication.class, args);
+
+	}
+
+}
