3a927ca581a2f17eacbdfdddbd4eef68b6a5c3d1
i261171 <laurent_bona@bcbstx.com>
2020-01-13 22:34:20 -0600

    saved

---
+++ javabriansData/src/main/java/io/javabrians/data/JavabriansDataApplication.java
@@ -0,0 +1,13 @@
+package io.javabrians.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavabriansDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavabriansDataApplication.class, args);
+	}
+
+}
